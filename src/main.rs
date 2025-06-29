a536405a5f6ad74b97c01a85eb17bf2566aac404
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2025-06-29 12:30:18 +0530

    copying a single file from src to destination

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    // GOAL: copy a single file from source to destination
+    println!("copy a single file from source to destination");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <src> <dest>", args[0]);
+        process::exit(1);
+    }
+
+    match transfer_file(&args[1], &args[2]) {
+        Ok(msg) => println!("{}", msg),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
+    }
+}
+
+fn transfer_file(src: &str, dest: &str) -> Result<String, String> {
+    match fs::copy(src, dest) {
+        Ok(bytes_copied) => Ok(format!(
+            "total bytes copied from {} to {}: {}",
+            src, dest, bytes_copied
+        )),
+        Err(e) => Err(format!("Transfer failed: {}", e)),
+    }
 }
